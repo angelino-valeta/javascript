99a90eeacba16be431225f8403f14caf0bc35b81
Angelino Valeta <angelinovaleta@gmail.com>
2020-02-08 22:23:24 +0100

    code function verific

--- practice/whoIs/script.js
+++ practice/whoIs/script.js
@@ -33,21 +33,25 @@ function verific(){
                 
                 gen = "child"
                 //child
-                img.setAttribute('src', 'img/')
+                img.setAttribute('src', 'img/1.jpg')
                 
-            }else if(age >= 11 && age < 35){
+            }else if(age >= 11 && age <= 30){
                 
                 gen = "young"
                 //young
+                img.setAttribute('src', 'img/22.jpg')
                 
-            } else if(age >= 35 && age <= 49){
+                
+            } else if(age >= 31  && age <= 49){
                 gen = "lord"
                 //lord
+                img.setAttribute('src', 'img/30.jpg')
                       
             }else if (age >= 50){
                 
                 gen = "adult"
                 //adult
+                img.setAttribute('src', 'img/64.jpg')
             }
                 
                         
@@ -60,25 +64,36 @@ function verific(){
                 
                 gen = "child"
                 //child
+                img.setAttribute('src', 'img/2.jpg')
                 
-            }else if(age >= 11 && age < 35){
+            }else if(age >= 11 && age <= 30){
                 
                 gen = "young"
                 //young
+                img.setAttribute('src', 'img/94.jpg')
                 
-            } else if(age >= 35 && age <= 49){
-                gen = "lord"
+            } else if(age >= 31 && age <= 49){
+                gen = "lady"
                 //lord
+                img.setAttribute('src', 'img/18.jpg')
                       
             }else if (age >= 50){
                 
                 gen = "adult"
                 //adult
+                img.setAttribute('src', 'img/80.jpg')
             }
             
         }
         
+        img.style.width = '200px'
+        img.style.height = '200px'
+        img.style.borderRadius = '50%'
+        img.style.marginTop = '10px'
+        img.style.boxShadow = '0 4px 8px 0 rgba(235, 47, 6, 0.2), 0 6px 20px 0 rgba(235, 47, 6, 0.19)'
+        
         answer.innerText = `checked ${gen} with ${age} year old.`
+        answer.appendChild(img)
     }
     
     
