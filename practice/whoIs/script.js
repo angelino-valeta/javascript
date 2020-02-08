e2711d87c739937a0a210abff2d8319f4cabdb53
Angelino Valeta <angelinovaleta@gmail.com>
2020-02-08 19:51:44 +0100

    code function verific

--- practice/whoIs/script.js
+++ practice/whoIs/script.js
@@ -2,7 +2,37 @@ var btn = window.document.getElementById('check')
 btn.addEventListener('click' , verific)
 
 function verific(){
-
-
+    
+    var gender = document.getElementsByName('gender')
+    
+    var yb = document.getElementById('yearbirth')
+    var yearbirth = Number(yb.value);
+    var answer = document.getElementById('answer')
+    var yearC = new Date().getFullYear()
+    
+    var age = yearC - yearbirth 
+    
+    
+    
+    if(yearbirth == 0 || yearbirth > yearC) {
+      
+        alert('error: check the datas')
+    }else{
+        
+        var gen = '';
+        
+        if(gender[0].checked){
+            gen = "man"
+                        
+                    
+        } else if(gender[1].checked){
+            
+            gen = "woman"
+            
+        }
+        
+        answer.innerText = `checked ${gen} with ${age} year old.`
+    }
+    
     
 }
