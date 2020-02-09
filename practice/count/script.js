c93def0e6718e1d20eba5f5558f7e3c234b71239
Angelino Valeta <angelinovaleta@gmail.com>
2020-02-09 19:09:39 +0100

    create function count

---
+++ practice/count/script.js
@@ -0,0 +1,23 @@
+
+var btn = document.querySelector('#btn')
+btn.addEventListener('click', count)
+
+function count(){
+    
+    var first = Number(document.getElementById('start').value)
+    var finish = Number(document.getElementById('finish').value)
+    var answer = document.getElementById('answer')
+    
+        
+   if(){
+        
+        for(first = 1; first <= finish; first++){
+        
+        answer.innerText = first
+        
+        
+    }
+      } 
+    
+    
+}
