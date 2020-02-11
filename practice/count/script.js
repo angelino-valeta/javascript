969d65a452d9fb1934139f1ecb8e48b3822fab94
Angelino Valeta <angelinovaleta@gmail.com>
2020-02-11 23:52:56 +0100

    done count, first < finish

--- practice/count/script.js
+++ practice/count/script.js
@@ -10,10 +10,24 @@ function count(){
     var answer = document.getElementById('answer')
     
     var counting = document.createElement('p')
+    answer.appendChild(counting)
     
+    answer.innerHTML = 'counting <br>'
     if(first == 0 || finish == 0 || step == 0){
         
         answer.innerText = 'cannot count, types data correctly'
+    }else{
+        
+        if(first < finish){
+            
+            for(i = first; i <= finish; i++){
+                
+                answer.innerText += `${i}` 
+                
+               
+            }
+        }
+        
     }
     
        
