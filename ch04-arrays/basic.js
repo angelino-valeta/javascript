2666eb4b680411028c43d9c87ef54dd2aec8c60d
Angelino Valeta <angelinovaleta@gmail.com>
2020-02-12 12:14:28 +0100

    write feature basic of arrays

---
+++ ch04-arrays/basic.js
@@ -0,0 +1,6 @@
+var num = [5,6,8,3,12,9]
+
+num.push(19) // add new element
+num.sort() // order
+console.log(`this arrys have ${num.lenght} elements` )
+console.log(`the first element of array is ${num[0]}`)
