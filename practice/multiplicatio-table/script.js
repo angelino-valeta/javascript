45507d92e253b5c9169ce2c69aeb89149d5f5764
Angelino Valeta <angelinovaleta@gmail.com>
2020-02-12 08:37:55 +0100

    done function multiTable

--- practice/multiplicatio-table/script.js
+++ practice/multiplicatio-table/script.js
@@ -6,6 +6,19 @@ function multiTable(){
     var num = Number(document.getElementById('number').value)
     var answer = document.getElementById('answer')
     
-    answer.innerHTML = num;
+    if(num == 0){
+        alert('field number is empty')
+    }else{
+        
+        for(var i = 1 ; i <= 10; i++){
+            
+            var table = document.createElement('option')
+            
+            table.text = `${num} x ${i} = ${num * i}`;
+            answer.appendChild(table)
+            
+        }
+    }
+    
     
 }
