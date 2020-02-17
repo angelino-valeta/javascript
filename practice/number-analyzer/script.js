63a4eb53f921d5e5a2ff30b1df038881f7c10bcd
Angelino Valeta <angelinovaleta@gmail.com>
2020-02-17 12:33:35 +0100

    function numberAnalyzer: add number inside tag select

--- practice/number-analyzer/script.js
+++ practice/number-analyzer/script.js
@@ -2,10 +2,20 @@ var btnAdd = document.getElementById('btnAdd')
 btnAdd.addEventListener('click', numberAnalyzer)
 
 function numberAnalyzer(){
-    var num = NUmber(document.getElementIdBy('number').value)
+    var num = Number(document.getElementById('number').value)
     var btnFinish = document.getElementById('btnFinish')
     var answer = document.getElementById('answer')
     
-    
-   
+    if(num == 0){
+        
+        alert('Number invalid. please type a number')
+        
+    }else{
+        
+        var opt = document.createElement('option')
+        
+        opt.text = num
+        answer.appedChild(opt)
+    }
+       
 }
