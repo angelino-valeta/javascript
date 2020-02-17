d843bff101041336c8d27394d5613fd8852b3589
Angelino Valeta <angelinovaleta@gmail.com>
2020-02-17 11:48:42 +0100

    get elements and create the function numberAnalyze

---
+++ practice/number-analyzer/script.js
@@ -0,0 +1,10 @@
+var btnAdd = document.getElementById('btnAdd')
+btnAdd.addEventListener('click', numberAnalyzer)
+
+function numberAnalyzer(){
+   
+    var btnFinish = document.getElementById('btnFinish')
+    var answer = document.getElementById('answer')
+    
+   
+}
