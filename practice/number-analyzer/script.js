759c3e64b934aa4c8ca0f24942529b321a994fb4
Angelino Valeta <angelinovaleta@gmail.com>
2020-02-17 11:51:38 +0100

    get element #number

--- practice/number-analyzer/script.js
+++ practice/number-analyzer/script.js
@@ -2,9 +2,10 @@ var btnAdd = document.getElementById('btnAdd')
 btnAdd.addEventListener('click', numberAnalyzer)
 
 function numberAnalyzer(){
-   
+    var num = NUmber(document.getElementIdBy('number').value)
     var btnFinish = document.getElementById('btnFinish')
     var answer = document.getElementById('answer')
+    
     
    
 }
