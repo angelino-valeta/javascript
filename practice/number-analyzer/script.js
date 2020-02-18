6a4e030f796f5d1d3130d8a953bc25bb79e27b25
Angelino Valeta <angelinovaleta@gmail.com>
2020-02-18 09:21:25 +0100

    done function numberaAnalyze and function finish

--- practice/number-analyzer/script.js
+++ practice/number-analyzer/script.js
@@ -2,11 +2,13 @@ var btnAdd = document.getElementById('btnAdd')
 var btnFinish = document.getElementById('btnFinish')
 var num = document.getElementById('number')
 var otherAnswer = document.getElementById('otherAnswer')
-var numbers = []
+
 
 
 btnAdd.addEventListener('click', numberAnalyzer)
 btnFinish.addEventListener('click', finish)
+
+var numbers = []
 
 function isNumber(n){
     if(Number(n) >= 1 && Number(n) <= 100){
@@ -34,7 +36,7 @@ function numberAnalyzer(){
     
     if(isNumber(num.value) && !isList(num.value, numbers) ){
         
-        numbers.push(num.value)
+        numbers.push(Number(num.value))
         
         var opt = document.createElement('option')
         opt.text += `you add number ${num.value}`
@@ -42,7 +44,6 @@ function numberAnalyzer(){
         
         
     }else{
-        
         
         alert('Number invalid. please type a number')
        
@@ -61,7 +62,7 @@ function finish(){
         alert('add values before finish')
     }else{
         otherAnswer.style.fontSize = '12px'
-        otherAnswer.style.textAlign = 'center'
+        otherAnswer.style.textAlign = 'left'
         
         var tot = numbers.length
         var bigger = numbers[0]
@@ -69,7 +70,7 @@ function finish(){
         var sum = 0
         var average = 0
         for(var i in numbers){
-            sum += numbers[i]
+            sum = sum + numbers[i]
             if(numbers[i] > bigger){
                 bigger = numbers[i]
             }
@@ -85,7 +86,7 @@ function finish(){
         otherAnswer.innerHTML += `We have ${tot} numbers ${'<br>'}`
         otherAnswer.innerHTML += `The less number is ${less} ${'<br>'}`
         otherAnswer.innerHTML += `The bigger number is ${bigger} ${'<br>'}`
-        otherAnswer.innerHTML += `The sum of numbers id ${sum} ${'<br>'}`
+        otherAnswer.innerHTML += `The sum of numbers is ${sum} ${'<br>'}`
         otherAnswer.innerHTML += `The avarage of numbers is ${average.toFixed(2)} ${'<br>'}`
         
         
