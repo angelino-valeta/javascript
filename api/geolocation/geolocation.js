878f97c7abb515938d4f80164c9de99e54723eb5
Angelino Valeta <angelinovaleta@gmail.com>
2020-02-24 00:02:07 +0100

    method call showError()

--- api/geolocation/geolocation.js
+++ api/geolocation/geolocation.js
@@ -2,7 +2,7 @@ var demo =  document.getElementById('demo')
 
 function getLocation(){
 	if(navigator.geolocation){
-		 navigator.geolocation.getCurrentPosition(showPosition);
+		 navigator.geolocation.getCurrentPosition(showPosition, showError);
         
 	}else{
 		demo.innerHTML = 'Geolocation is not supported by your browser.'
@@ -12,3 +12,21 @@ function getLocation(){
 function showPosition(position){
     	demo.innerHTML = "Latitude: " + position.coords.latitude + "<b>Longitude:" + position.coords.longitude;
     }
+
+function showError(error){
+	switch(error){
+		case error.PERMISSION_DENIED:
+			demo.innerHTML = "User denied the request for Geolocation."
+			break;
+		case error.POSITION_UNAVALIABLE:
+			demo.innerHTML = "Location information is unavaliable."
+			break;
+		case error.TIMEOUT:
+			demo.innerHTML = "The request to get user location time out."
+			break;
+		case error.UNKNOWN_ERROR:
+			demo.innerHTML = "An unknown error occurred."
+			break;
+	
+	}
+}
