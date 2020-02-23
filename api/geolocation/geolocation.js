42c44294309fb3e0d353b783160beefe89d58d7f
Angelino Valeta <angelinovaleta@gmail.com>
2020-02-24 00:17:46 +0100

    watchPosition

--- api/geolocation/geolocation.js
+++ api/geolocation/geolocation.js
@@ -3,7 +3,7 @@ var demo =  document.getElementById('demo')
 function getLocation(){
 	if(navigator.geolocation){
 		 navigator.geolocation.getCurrentPosition(showPosition, showError);
-        
+     		// navigator.geolocation.watchPosition(showPosition);   
 	}else{
 		demo.innerHTML = 'Geolocation is not supported by your browser.'
 	}
