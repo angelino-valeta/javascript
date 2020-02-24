dc94507e300f12ebde96da729fc2de58a307b4a5
Angelino Valeta <angelinovaleta@gmail.com>
2020-02-24 14:15:57 +0100

    removing localStorage

--- api/webStorage/webstorage.js
+++ api/webStorage/webstorage.js
@@ -4,6 +4,7 @@ var result = document.getElementById('result');
 if(typeof(Storage) !== "undefined"){
 	// Storage
 	localStorage.setItem("lastname", "Valeta");
+	localStorage.removeItem("lastname");
 	result.innerHTML = localStorage.getItem("lastname");
 }else{
 	result.innerHTML = "Sorry, your browser does not support web storage";
